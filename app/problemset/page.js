46f25f0974354ce8555befc8415317544a5a433a
iwannabethesophia <pydevmonkey4002@gmail.com>
2023-12-10 23:11:23 +0700

    clean unnecessary className

--- app/problemset/page.js
+++ app/problemset/page.js
@@ -49,7 +49,7 @@ export default function ProblemsetPage() {
                 </div>
                 <div className="absolute right-0 mr-6">
                   <Link href={`/problemset/${problemset.id}`}>
-                    <IconButton className="bg-white text-[#0066ff] border-2 border-[#0066ff] hover:border-blue-700 hover:text-white hover:bg-blue-700 transition duration-300 px-4 py-2 rounded-xl">
+                    <IconButton className="bg-white text-[#0066ff] border-2 border-[#0066ff] hover:border-blue-700 hover:text-white hover:bg-blue-700 transition duration-300 px-4 py-2">
                       Challenge
                       <FaArrowRight className="ml-2"/>
                     </IconButton>
