ecf3105897ffcebbabe620a00838b2edad58f446
iwannabethesophia <pydevmonkey4002@gmail.com>
2023-12-12 05:26:41 +0700

    update: done pagination function

--- app/problemset/page.tsx
+++ app/problemset/page.tsx
@@ -40,7 +40,7 @@ const paginationTheme: CustomFlowbiteTheme = {
 
 export default function ProblemsetPage() {
 
-  const test_data = [
+  const test_data: Array<any> = [
     {"id": "licodeprogrammingcontest2", "name": "LICODE PROGRAMMING CONTEST #2", "authors": ["ecnerwala", "Errichto"], "avg_diff": 3282.1, "tag": ["geometry", "fft", "adhoc"]},
     {"id": "licodeprogrammingcontest1", "name": "LICODE PROGRAMMING CONTEST #1", "authors": ["iwannabetheguy", "tourist"], "avg_diff": 2754.6, "tag": ["graph", "geometry"]},
     {"id": "licodeprogrammingcontest2", "name": "LICODE PROGRAMMING CONTEST #2", "authors": ["ecnerwala", "Errichto"], "avg_diff": 3282.1, "tag": ["geometry", "fft", "adhoc"]},
@@ -76,66 +76,89 @@ export default function ProblemsetPage() {
   ];
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [pageOffset, setPageOffset] = useState(10);
+  const totalPages: number = pageOffset === 0 ? 0 : parseInt((test_data.length / pageOffset).toString()) + 1;
 
   const onPageChange = (page: number) => setCurrentPage(page);
-  console.log(test_data.length);
 
   return (
-    <div className="flex flex-col justify-center px-16">
-      <div className="mb-4">
-        <h2 className="font-sans text-2xl font-bold">LIST OF PROBLEMSET</h2>
-      </div>
-      <div className="grid grid-cols-3 gap-4 w-full">
-        <div className="relative col-span-2">
-          {
-            test_data.map((problemset, idx) => (
-              <Card className="max-w-full mb-2">
-                <Link href={`/problemset/${problemset.id}`} className="text-xl font-sans font-bold tracking-tight text-gray-900">
-                  <span className="hover:text-[#0066ff] hover:underline hover:underline-offset-4">{(idx + 1) + ". " + problemset.name}</span>
-                </Link>
-                <div className="font-normal text-gray-700 flex-col">
-                  <div className="text-sm align-middle flex">Difficulity: <Badge className="ml-1 rounded-lg" color="failure">{problemset.avg_diff}</Badge></div>
-                  <div className="text-sm align-middle flex mt-1">
-                    Problemset tag:
-                    {
-                      problemset.tag.map(t => (
-                       <Badge className="ml-1 rounded-lg" color="info">{t}</Badge>
-                      ))
-                    }
-                  </div>
-                  <div className="text-sm align-middle flex mt-1">
-                    Authors:
-                    {
-                      problemset.authors.map(author => (
-                       <Badge className="ml-1 rounded-lg" color="gray">{author}</Badge>
-                      ))
-                    }
-                  </div>
-                </div>
-                <div className="absolute right-0 mr-6">
-                  <Link href={`/problemset/${problemset.id}`}>
-                    <IconButton className="bg-white text-[#0066ff] border-2 border-[#0066ff] hover:border-blue-700 hover:text-white hover:bg-blue-700 transition duration-300 px-4 py-2">
-                      Challenge
-                      <FaArrowRight className="ml-2"/>
-                    </IconButton>
-                  </Link>
-                </div>
-              </Card>
-            ))
-          }
-          <div className="flex overflow-x-auto sm:justify-center">
-            <Pagination currentPage={currentPage} totalPages={parseInt(test_data.length / 10 + 1)} onPageChange={onPageChange} showIcons theme={paginationTheme} />
+  <div className="flex flex-col justify-center px-16">
+    <div className="mb-4">
+      <h2 className="font-sans text-2xl font-bold">LIST OF PROBLEMSET</h2>
+    </div>
+    <div className="grid grid-cols-3 gap-4 w-full">
+      <div className="relative col-span-2">
+        <div className="flex justify-between items-center overflow-x-auto mb-4">
+          <div className="items-center justify-center">
+            <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} theme={paginationTheme} showIcons />
+          </div>
+          <div className="items-center justify-end">
+            <label>Entries:</label>
+            <input className="ml-2 outline-none bg-slate-100 px-4 py-2 w-16 rounded-xl hover:bg-slate-200" value={pageOffset} onChange={event => setPageOffset(event.target.value == "" ? 0 : parseInt(event.target.value.toString()))} />
           </div>
         </div>
-        <div>
-          <Card className="max-w-full">
-            <h5 className="text-xl font-sans font-bold tracking-tight text-gray-900">
-              Search problemset
-            </h5>
-            <div className="font-normal text-gray-400">Coming soon...</div>
+        {
+          pageOffset == 0 || test_data.length == 0 ?
+          <Card className="my-4">
+            <h2 className="text-center text-3xl font-sans font-bold py-8">No problemset found.</h2>
           </Card>
+          :
+          test_data
+            .slice((currentPage - 1) * pageOffset, currentPage * pageOffset)
+            .map((problemset, idx) => (
+            <Card className="max-w-full mb-2">
+              <Link href={`/problemset/${problemset.id}`} className="text-xl font-sans font-bold tracking-tight text-gray-900">
+                <span className="hover:text-[#0066ff] hover:underline hover:underline-offset-4">{(idx + 1) + ". " + problemset.name}</span>
+              </Link>
+              <div className="font-normal text-gray-700 flex-col">
+                <div className="text-sm align-middle flex">Difficulity: <Badge className="ml-1 rounded-lg" color="failure">{problemset.avg_diff}</Badge></div>
+                <div className="text-sm align-middle flex mt-1">
+                  Problemset tag:
+                  {
+                    problemset.tag.map(t => (
+                     <Badge className="ml-1 rounded-lg" color="info">{t}</Badge>
+                    ))
+                  }
+                </div>
+                <div className="text-sm align-middle flex mt-1">
+                  Authors:
+                  {
+                    problemset.authors.map(author => (
+                     <Badge className="ml-1 rounded-lg" color="gray">{author}</Badge>
+                    ))
+                  }
+                </div>
+              </div>
+              <div className="absolute right-0 mr-6">
+                <Link href={`/problemset/${problemset.id}`}>
+                  <IconButton className="bg-white text-[#0066ff] border-2 border-[#0066ff] hover:border-blue-700 hover:text-white hover:bg-blue-700 transition duration-300 px-4 py-2">
+                    Challenge
+                    <FaArrowRight className="ml-2"/>
+                  </IconButton>
+                </Link>
+              </div>
+            </Card>
+          ))
+        }
+        <div className="flex justify-between items-center overflow-x-auto mb-4">
+          <div className="items-center justify-center">
+            <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} theme={paginationTheme} showIcons />
+          </div>
+          <div className="items-center justify-end">
+            <label>Entries:</label>
+            <input className="ml-2 outline-none bg-slate-100 px-4 py-2 w-16 rounded-xl hover:bg-slate-200" value={pageOffset} onChange={event => setPageOffset(event.target.value == "" ? 0 : parseInt(event.target.value.toString()))} />
+          </div>
         </div>
       </div>
+    <div>
+      <Card className="max-w-full">
+        <h5 className="text-xl font-sans font-bold tracking-tight text-gray-900">
+          Search problemset
+        </h5>
+        <div className="font-normal text-gray-400">Coming soon...</div>
+      </Card>
     </div>
+  </div>
+  </div>
   )
 }
