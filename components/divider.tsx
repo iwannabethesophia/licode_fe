76830a2e1c267e5c5691bf4bba7f29250a50cffa
iwannabethesophia <pydevmonkey4002@gmail.com>
2023-12-23 22:02:10 +0700

    add: TextDivider for better header display of section

--- components/divider.tsx
+++ components/divider.tsx
@@ -9,4 +9,18 @@ function DividerX({height=6, className="", color=""}) {
   )
 }
 
-export { DividerX };
+function TextDivider({ hrStyle="", textPlacement="center", children, ...attribute }) {
+  const placementStyle: any = {
+    "left"  : "start-0 pr-4",
+    "center": "left-1/2 -translate-x-1/2 px-4",
+    "right":  "end-0 pl-4",
+  }
+  return (
+    <div className="inline-flex items-center justify-center w-full relative">
+      <hr className={twMerge("w-full h-px my-4 bg-gray-300 border-0", hrStyle)} />
+      <div className={twMerge("absolute bg-white", placementStyle[textPlacement])}>{children}</div>
+    </div>
+  )
+}
+
+export { DividerX, TextDivider };
